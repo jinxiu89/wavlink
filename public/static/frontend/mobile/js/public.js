2aa23add64967441674139189b55a65aa5c7c74f
Alan <1367428189@qq.com>
2019-11-30 17:56:18 +0800

    搜索组件加遮罩层

--- public/static/frontend/mobile/js/public.js
+++ public/static/frontend/mobile/js/public.js
@@ -1,3 +1,30 @@
+function gFoot () {
+    //footer交互效果
+    $(".about-item h3").click(function () {
+        $(this).siblings("ul").stop(true, false).slideToggle();
+        $(this).children("span").toggleClass("iconclose").toggleClass("iconincrease");
+        $(this).parent("li").siblings("li").children("ul").stop(true, false).slideUp();
+        $(this).parent("li").siblings("li").children("h3").find("span").addClass("iconincrease").removeClass("iconclose");
+    })
+}
+function gSearch () {
+    //搜索交互
+    var searchBar = $(".search-bar"),
+        searchForm = $(".search-container form"),
+        searchMark = $(".search-container .search-mask-layer");
+    searchBar.click(function (e) {
+        searchForm.stop(false, true).fadeToggle(300);
+        searchMark.stop(false, true).fadeToggle(300);
+        e.stopPropagation();
+    });
+    $(".search-container input").click(function (e) {
+        e.stopPropagation();
+    });
+    $(document).add(".menu-bar>.toggle").click(function () {
+        searchForm.fadeOut(300);
+        searchMark.fadeOut(300);
+    });
+}
 function goTop () {
     var go_top = $("#go-top"),                // 侧边导航;
         go_topP = $("#go-top p");// 侧边导航 回到顶部按钮;
@@ -34,8 +61,9 @@ function gPath () {
         })
     }
 }
-
 $(document).ready(function () {
+    gFoot();
+    gSearch();
     goTop();
     gPath();
 });

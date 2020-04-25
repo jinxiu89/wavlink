da5dde12f9862d385ec6fc5079d7fe19d190c1dc
jinxiu89 <jinxiu89@163.com>
2020-04-25 17:34:59 +0800

    阿里云OSS上传

--- application/lib/utils/cloud/ali.php
+++ application/lib/utils/cloud/ali.php
@@ -10,6 +10,7 @@
  **/
 
 namespace app\lib\utils\cloud;
+use OSS\Model\ObjectListInfo;
 use OSS\OssClient;
 use OSS\Core\OssException;
 use OSS\Core\OssUtil;
@@ -32,8 +33,37 @@ class ali
             //todo: 抛出异常
         }
     }
-    public function putFile(){
 
+    /***
+     * 列出 桶里的数据
+     * 后面使用的时候再来丰富注释和功能
+     * @param string $bucket
+     * @return ObjectListInfo|string
+     *
+     */
+    public function listObj($bucket='wavlink'){
+        $instance=self::createClient();
+        try{
+            $data=$instance->listObjects($bucket);
+            if(!empty($data)){
+                return $data;
+            }
+        }catch (OssException $exception){
+            return $exception->getMessage();
+        }
     }
 
+    /**
+     * @param string $bucket
+     * @param string $object
+     * @param $file
+     */
+    public function putFile($bucket='wavlink',$object='',$file){
+        $instance=self::createClient();
+        try{
+            $instance->uploadFile($bucket,$object,$file);
+        }catch (OssException $exception){
+
+        }
+    }
 }

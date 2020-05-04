c646f48de6d86bbde0153a3b7c31866bfe638250
Alan <1367428189@qq.com>
2020-05-04 15:43:35 +0800

    update

--- public/static/customer/ui/js/public.js
+++ public/static/customer/ui/js/public.js
@@ -28,13 +28,15 @@ function layer_open(title, url, w, h) {
         h = ($(window).height() - 50);
     }
     layer.open({
+        skin: 'wavlink-layer',
         type: 2,
         area: [w + 'px', h + 'px'],
         fix: false, //不固定
         maxmin: true,
         shade: 0.4,
         title: title,
-        content: url
+        content: url,
+        shadeClose: true
     });
 }
 
